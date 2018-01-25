412af68488eeb4c2c5f84b09dbbfbb24e4c12fea
corast <corastweb94@hotmail.com>
2018-01-25 12:51:11 +0100

    Task 2, check mean square error rate
    Task 2, finishing up + starting Task 3, plotting with a new data set

--- LinearRegression/main.py
+++ LinearRegression/main.py
@@ -1,6 +1,7 @@
 #Assignment 1 Linear Regression
 
 import numpy as np
+import matplotlib.pyplot as pyplot
 
 """
 A = np.matrix([[1, 0.2],[1, 2.4],[1, 2.2],[1, 1.1],[1, 0.6]])
@@ -28,24 +29,55 @@ def init(data):
     return X,Y
 
 def train(data):
-    #X matris which is an matrix of all training examples.
+    """ Train Linear Regression algorithm with normal equation on input data"""
+    #X matrix is an matrix of all training examples.
     X,Y = init(data)
     
     #print(X.transpose())
     """ Task 1: Implement OLS (Normal equation)  """
     W = (((X.transpose()*X).I)*X.transpose())*Y
     return W
+
 #Task 1.
-trainData = np.loadtxt(open("../data/regression/test_2d_reg_data.csv", "rb"), delimiter=",")
+trainData = np.loadtxt(open("../regression/train_2d_reg_data.csv", "rb"), delimiter=",")
 #Load all the data into an matrix, for easy use later. Optimaly we would push to an matrix
 
 #Task 2, train and test.
-testData = np.loadtxt(open("../data/regression/train_2d_reg_data.csv", "rb"), delimiter=",")
+testData = np.loadtxt(open("../regression/test_2d_reg_data.csv", "rb"), delimiter=",")
 
-def test(weights, data):
-    error = 0
+def test(W, data):
+    X,Y = init(data)
 
-    for test in data:
-        pass 
+    #Equation 6 
+    Emse = (X*W - Y).transpose()*(X*W-Y)
 
+    return Emse
+
+#First we train and calculate weights
+Weights = train(testData)
+Error =  test(Weights, testData)
+#np.squeeze(np.asarray(Weights[0])
+#Show weights and error after testing
+print("w_0 =  {0:.4f}, w_1 = {1:.4f}, w_2 = {2:.4f}, Model error: E_mse(w) = {3:4f}".format(
+    np.squeeze(np.asarray(Weights[0])), 
+    np.squeeze(np.asarray(Weights[1])), 
+    np.squeeze(np.asarray(Weights[2])), 
+    np.squeeze(np.asarray(Error))))
+
+#Task 3, plot line after training and plot values
+
+tast3_trainingData = trainData = np.loadtxt(open("../regression/train_1d_reg_data.csv", "rb"), delimiter=",")
+
+W_t3 = train(tast3_trainingData)
+
+def h(x):
+    return np.squeeze(np.asarray(W_t3[0])) + (np.squeeze(np.asarray(W_t3[1])))*x
 #mathlibplot
+
+#import mathlibplot.pyplot as plot
+
+
+x = np.arange(0.0, 2.0, 0.1)
+
+pyplot.plot(x, h(x),'r--')
+pyplot.show()
