54f3140ad9cc2980832899ca6fca9ecab0ad7a73
corast <corastweb94@hotmail.com>
2018-01-29 16:08:54 +0100

    Finishing up code with comments
    Comment and clean up code

--- code/linearRegression.py
+++ code/linearRegression.py
@@ -3,15 +3,8 @@
 import numpy as np
 import matplotlib.pyplot as pyplot
 
-"""
-A = np.matrix([[1, 0.2],[1, 2.4],[1, 2.2],[1, 1.1],[1, 0.6]])
-B = np.matrix([[1],[2],[3],[4],[5]])
-print(A.transpose())
-print(A.transpose()*A)
-print(A.transpose()*B)
-C = A.transpose()
-"""
 def init(data):
+    """ Input is the data array, with one example per row, which we split up to seperate X and Y matrixes. And return as numpy matrix objects, for easy matrix calculations"""
     X = []
     Y = []
 
@@ -30,22 +23,22 @@ def init(data):
 
 def train(data):
     """ Train Linear Regression algorithm with normal equation on input data"""
-    #X matrix is an matrix of all training examples.
+    #X matrix is an matrix of all training examples, Y corresponding correct answer.
     X,Y = init(data)
     
-    #print(X.transpose())
     """ Task 1: Implement OLS (Normal equation)  """
     W = (((X.transpose()*X).I)*X.transpose())*Y
     return W
 
 #Task 1.
 trainData = np.loadtxt(open("../regression/train_2d_reg_data.csv", "rb"), delimiter=",")
-#Load all the data into an matrix, for easy use later. Optimaly we would push to an matrix
+#Load all the data into an matrix, for easy use later. Optimaly we would push to an matrix in the same step.
 
 #Task 2, train and test.
 testData = np.loadtxt(open("../regression/test_2d_reg_data.csv", "rb"), delimiter=",")
 
 def test(W, data):
+    """ Takes the trained weights W, and data to test this on. Return the mean squared error """
     X,Y = init(data)
 
     #Equation 6 
@@ -57,15 +50,16 @@ def test(W, data):
 Weights = train(trainData)
 
 
+#Print the trained weights.
 print(np.asarray(Weights))
 
-#np.squeeze(np.asarray(Weights[0])
 #Show weights and error after testing
 print("w_0 =  {0}, w_1 = {1}, w_2 = {2}".format(
     np.squeeze(np.asarray(Weights[0])), 
     np.squeeze(np.asarray(Weights[1])), 
     np.squeeze(np.asarray(Weights[2]))))
 
+#Calculate Error for the testing data and training data
 Error = test(Weights, testData)
 print("Error: testData = ",end = "")
 print(np.asscalar(Error))
@@ -74,22 +68,24 @@ print("Error: trainData =  ",end = "")
 print(np.asscalar(Error_test))
 
 #Task 3, plot line after training and plot values
+""" Task 3, plot boundary line after traing with data """
 
 def init_plot(data):
-    """ Split test examples into X and Y array, for easy plotting """
+    """ Split test examples into X and Y array, for easy plotting later. Could have plottet this right away here too"""
     X = []
     Y = []
 
     for example in data:
         Y.append(example[1])
         X.append(example[0])
-    
     return X,Y
 
+"""Load the test and training data required for task 3 """ 
 trainingData_t3 = np.loadtxt(open("../regression/train_1d_reg_data.csv", "rb"), delimiter=",")
 
 testData_t3 = np.loadtxt(open("../regression/test_1d_reg_data.csv", "rb"), delimiter=",")
 
+#Calculat the weights on the trainingdata.
 W_t3 = train(trainingData_t3)
 
 #Function that we want to plot(linear function) with our trained weights.
--- code/logisticRegression.py
+++ code/logisticRegression.py
@@ -1,20 +1,14 @@
 #Assignment 1 Logistic Regression
 import numpy as np
 import matplotlib.pyplot as pyplot
-#def h(X,W):
-#    return W.transpose()*X
 
 def sigmoid(z):
+    """ Calculate the sigmoid value """
     return 1/(1+np.exp(-z))
 
-def predict_function(z):
-    """ Predicts an discrete output based on input"""
-    if (sigmoid(z) >= 0.5):
-        return 1
-    return 0
-
 def init(data):
-    """ Initialize data sets into workable matrixes."""
+    """ Initialize data sets into workable seperate matrixes as before.
+    X = [[1 x1 x2], [1 x1 x2],...],  Y = [y1,y2,...]"""
     X = []
     Y = []
 
@@ -32,6 +26,7 @@ def init(data):
     return X,Y
 
 def init_weights(n):
+    """ Initialize the pre trained weights, all zeros in our case. W = [[w0],[w1],..,[wn]] """
     W = []
     for i in range(n):
         W.append([0])
@@ -42,8 +37,10 @@ def init_weights(n):
 #Error_k = []
 
 def error_N(W, X, Y):
+    """ Calculate the error for all the examples X per itteration we are given. Sum of error for all functions."""
+    #N is an scalar keeping track of how many examples we got in X
     N = X.shape[0]
-    sume = 0
+    sume = 0 # start sum at 0.
     for i in range(N):
         #Itterate
         z = W.transpose()*X[i].transpose()
@@ -51,12 +48,14 @@ def error_N(W, X, Y):
     return ((-1)*np.asscalar(sume))/N
 
 def error(W,X,y):
-    """ Calculate error per example. """
+    """ Calculate error for one example. Only used for debugging """
     z = W.transpose()*X.transpose()
     error = y*np.log(sigmoid(z)) + (1-y)*np.log(1-sigmoid(z))
     return error
 
 def gradiendDecent(learning_rate, n_iterations, X, Y, e = False):
+    """ main function to find weights. """
+    #Initalise weights to all zeros
     W = init_weights(X.shape[1])
     #We need to itterate over m times, to train every weight
     E_ce = [] #so we can test different functions.
@@ -64,16 +63,15 @@ def gradiendDecent(learning_rate, n_iterations, X, Y, e = False):
     for n in range(n_iterations):
         sum_n = init_weights(X.shape[1])
         for i in range(X.shape[0]): #Itterate over every training example.
-            #Note the need for transposing X as well, this is because the way X matrix is represented with one example in every row 
-            #print("{} {} {} {} {} {} {}".format(X[1].shape, W.shape,Y.shape, Y.transpose().shape, X.transpose().shape))
-            z = W.transpose()*X[i].transpose()
+            #Note the need for transposing X as well, this is because the way X matrix is represented with one example in every row, instead of every colum as in the assignment paper.
+            z = W.transpose()*X[i].transpose() # z we pass to sigmoid function.
             sum_n = sum_n + np.asscalar(sigmoid(z) - Y[i])*X[i].transpose()
-
 
         #Update the weigth with this sum.
         tempW = W
         W = W - learning_rate*sum_n
         if(e):
+            """ If we are only after the error, but not the weights gained. Very slow and inefficient"""
             #Calculate error for k-th iteration.
             Eck_k = error_N(W,X,Y)
             E_ce.append(Eck_k)
@@ -83,6 +81,7 @@ def gradiendDecent(learning_rate, n_iterations, X, Y, e = False):
         #    return W
             #learning_rate *= 0.5 #half learning rate.
     if(e):
+        """ return error for every itteration, istead of weights trained. """
         #If we just want the error to plot, we instead call this function.
         return E_ce
         #Error_k.append(E_ce)
@@ -92,18 +91,13 @@ def gradiendDecent(learning_rate, n_iterations, X, Y, e = False):
 trainData = np.loadtxt(open("../classification/cl_train_1.csv", "rb"), delimiter=",")
 
 testData = np.loadtxt(open("../classification/cl_test_1.csv", "rb"), delimiter=",")
-#seperate_data
 
+#seperate_data
 X,Y = init(trainData) #Initialize data matrixes
 
 P,N = init(testData)
 
-#Initalise weights to all zeros
-#W = init_weights(X.shape[1])
-
 learning_rate = 0.1
-
-
 
 #Function that we want to plot(linear function) with our trained weights.
 def n(x):
@@ -112,26 +106,7 @@ def n(x):
 def p(x):
     return predict_function(W*x.transform())
 
-#Gather values to plot from test data
-#X_plot, Y_plot = init_plot(testData_t3)
-
-
-
-def error_N(W, X, Y):
-    N = X.shape[0]
-    sume = 0
-    for i in range(N):
-        #Itterate
-        z = W.transpose()*X[i].transpose()
-        sume += Y[i]*np.log(sigmoid(z)) + (1-np.asscalar(Y[i]))*np.log(1-sigmoid(z))
-    return ((-1)*np.asscalar(sume))/N
-
-def error(W,X,y):
-    """ Calculate error per example. """
-    z = W.transpose()*X.transpose()
-    error = y*np.log(sigmoid(z)) + (1-y)*np.log(1-sigmoid(z))
-    return error
-
+#Error value, if needed
 #print(error_N(W,X,Y))
 
 def plot_error(Error_k):
@@ -140,108 +115,92 @@ def plot_error(Error_k):
     x = np.arange(0, 1000, 1)
     #plot the data
     pyplot.plot(x, Error_k[0],'r--', x, Error_k[1], 'g--')
-    pyplot.legend(('Training set', 'Test set'),loc="upper right")
-    pyplot.ylim([0,1]) 
+    pyplot.legend(('Training set', 'Test set'),loc="upper right") #name the values
+    pyplot.ylim([0,1]) #Limit y value, between 0 and 1. x is already limited by np.arrange 
     pyplot.show()
 
-def decision_boundry(W, X):
-    #points where W^t*X = 0
-    boundry = W.transpose()*X.transpose()
-    #print(X.shape)
-    #print(W.shape)
-    print(boundry.shape)
-    return boundry.tolist()
-
-def b(x):
-    return -np.squeeze(np.asarray(W[0]))/np.squeeze(np.asarray(W[2])) - (np.squeeze(np.asarray(W[1])))/(np.squeeze(np.asarray(W[2])))*x
-
 def b(x,W):
+    """function that correspond to boundary with our weights"""
     return -np.squeeze(np.asarray(W[0]))/np.squeeze(np.asarray(W[2])) - (np.squeeze(np.asarray(W[1])))/(np.squeeze(np.asarray(W[2])))*x   
 
 def plot_data(data,W):
     """ Plot data as coloured points according to acctual value"""
     numberOfParameters = data[0].size   
     for example in data:
-        """ Check y value of each example and plot accordingly """
+        """ Check y value of each example and plot colour accordingly """
         if(example[numberOfParameters-1] == 1):
             pyplot.plot(example[0],example[1], "go")
         elif(example[numberOfParameters-1] == 0):
             pyplot.plot(example[0],example[1], "ro")
-    x = np.arange(0,1.1,0.1) #x values to plot the decision boarder.
+    x = np.arange(0,1.1,0.1) #x values to plot the decision bondary.
     pyplot.plot(x,b(x,W),'k--')
     pyplot.xlabel("x1")
     pyplot.ylabel("x2")
     pyplot.ylim([0,1])
-    pyplot.xlim([0,1])
     pyplot.show()
 
 def plot_b(data,W):
-    """ Plot data """
-    numberOfParameters = data[0].size   
-    """for example in data:
-        if(example[numberOfParameters-1] == 1):
-            pyplot.plot(example[0],example[1], "go")
-        elif(example[numberOfParameters-1] == 0):
-            pyplot.plot(example[0],example[1], "ro")
-    """
+    """ Plot boundary alone, for debugging"""
     x = np.arange(0,1.1,0.1)
     pyplot.plot(x,b(x,W),'k-')
-    #pyplot.xlabel("x1")
-    #pyplot.ylabel("x2")
-    #pyplot.ylim([0,1])
-    #pyplot.xlim([0,1])
+    pyplot.ylim([0,1])
     pyplot.show()
     
 
-#compute W on the training set
-#W = gradiendDecent(learning_rate, 1000, X, Y,False)
-
-#Print error function with each itteration, but very slow cause we need to run 3 times to compute w, and error plots.
+ #Commented older tasks.
 """
+#compute W on the training set with old weights 
+W = gradiendDecent(learning_rate, 1000, X, Y,False)
+"""
+
+""" #Print error function with each itteration, but very slow cause we need to run 3 times to compute w, and error plots.
 Error_k = gradiendDecent(learning_rate, 1000, X, Y, True), gradiendDecent(learning_rate,1000, P, N, True)
 plot_error(Error_k)
 """
-#Use plot_data to show boundry line on the data
+"""#Use plot_data to show boundry line on the data
 #plot_data(trainData,W)
 #plot_data(testData,W)
+"""
 
 """Task 2.2.2"""
 #read csv files
 trainData_2 = np.loadtxt(open("../classification/cl_train_2.csv", "rb"), delimiter=",")
 testData_2 = np.loadtxt(open("../classification/cl_test_2.csv", "rb"), delimiter=",")
 
-#X,Y = init(trainData_2)
-#X2,Y2 = init(testData_2)
-
+X,Y = init(trainData_2)
+X2,Y2 = init(testData_2)
+""" #Try to train weights with old boundary function 
 #W = gradiendDecent(0.01, 1000,X, Y)
-
 
 #error_value_test = error_N(W,X2,Y2)
 #print(error_value_test)
-
 #plot_data(testData_2,W)
 
 #Error_k = gradiendDecent(0.01, 1000, X, Y, True), gradiendDecent(0.01, 1000, X2, Y2, True)
+
 #plot_error(Error_k)
-
 #plot_data(trainData_2, W)
+"""
 
-""" Upscaling the h function to actually solve the function"""
+""" Upscaling the h function to actually solve the classification problem."""
 
 def b_2p(x,W): #Positiv solution
     """ Return one x_2 value to plot for an x_1 value, W[n]=wn""" 
+    """ Since x is an array of all the x1 values, it calculates every point in one swoop, but this gives us the problem of negative C values in the sqrt, which gives an warning. """
     A = np.asscalar(W[4])
     B = np.asscalar(W[2])
     C = np.asscalar(W[0]) + np.asscalar(W[1])*x + (np.asscalar(W[3]))*x**2
+    #Return an array with all points we need to plot for x2
     return (- B + np.sqrt(B**2 - 4*A*C) ) / (2*A)
 
 
 def b_2n(x,W): #Negative solution
     """ Return one x_2 value to plot for an x_1 value, W[n]=wn""" 
-    """ Since X is an array of all the values, it calculates every point in one swoop, but this gives us the problem of negative C values in the sqrt, which gives an warning. """
+    """ Since x is an array of all the values x1, it calculates every point in one swoop, but this gives us the problem of negative C values in the sqrt, which gives an warning. """
     A = np.asscalar(W[4])
     B = np.asscalar(W[2])
     C = np.asscalar(W[0])+np.asscalar(W[1])*x+(np.asscalar(W[3]))*x**2
+    #Return an array with all points we need to plot for x2
     return (- B - np.sqrt(B**2 - 4*A*C) ) / (2*A)
 
 def init_updated(data):
@@ -256,7 +215,7 @@ def init_updated(data):
         row = [1]
         for i in range(numberOfParameters-1):
             row.append(example[i])
-        #Add more variables
+        #Add more variables for x1 and x2
         for i in range(numberOfParameters-1):
             row.append(example[i]*example[i])
         X.append(row)
@@ -265,17 +224,16 @@ def init_updated(data):
     Y = np.matrix(Y)
     return X,Y
 
-X,Y = init_updated(trainData_2)
-X2,Y2 = init_updated(testData_2)
-
 #Train on training set, as before
 W = gradiendDecent(0.1, 1000,X, Y)
 
-#Error_k = gradiendDecent(0.1, 1000, X, Y, True), gradiendDecent(0.1, 1000, X2, Y2, True)
-#plot_error(Error_k)
+""" #Plot error function from both data examples
+Error_k = gradiendDecent(0.1, 1000, X, Y, True), gradiendDecent(0.1, 1000, X2, Y2, True)
+plot_error(Error_k)
+"""
 
 def plot_b(data, W):
-    """ Plot data """
+    """ Plot data with new boundary function """
     numberOfParameters = data[0].size
     for example in data:  
         if(example[numberOfParameters-1] == 1):
@@ -284,14 +242,13 @@ def plot_b(data, W):
             pyplot.plot(example[0],example[1], "ro")
 
     x = np.arange(0,1,0.00001)
-    #Plot boundary function
+    #Plot boundary function, and plot settings
     pyplot.legend(('Negative', 'Positiv'),loc="upper right")
     pyplot.plot(x,b_2p(x,W),'k-', x,b_2n(x,W),'k-')
     pyplot.xlabel("x1")
     pyplot.ylabel("x2")
     pyplot.ylim([0,1])
-    pyplot.xlim([0,1])
     pyplot.show()
 
+#Run final plot function.
 plot_b(testData_2,W)
-#print(b_2n(0,W))
